9516aae3593c0dc15a754630f53601179d3f2d46
Nestor Soto <nestor.soto@unmsm.edu.pe>
2022-07-16 09:45:25 -0500

    descripcion v1

--- HoloTourism/lib/resources/Imagen.dart
+++ HoloTourism/lib/resources/Imagen.dart
@@ -2,28 +2,33 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:untitled1/models/lugar_model.dart';
+import 'package:untitled1/httpResponses/entities/Lugar.dart';
 import 'dart:io';
+
+/*
+*
+* CLASE PARA MANEJO DE IMAGENES
+*
+* */
 
 class Imagen {
   Future<String> subir(String field, String filePath) async {
-    var request = http.MultipartRequest("POST", Uri.parse('http://192.168.1.2:4000/api/images/saveimg/'));
+    var request = http.MultipartRequest("POST",
+        Uri.parse('https://holotourism.herokuapp.com/api/images/saveimg/'));
     request.fields["file"] = field;
     request.files.add(await http.MultipartFile.fromPath("file", filePath));
-    request.headers.addAll({
-      "Content-type": "multipart/form-data"
-    });
+    request.headers.addAll({"Content-type": "multipart/form-data"});
     var response = await request.send();
-    if (response.statusCode == 200){
+    if (response.statusCode == 200) {
       return 'ok';
     } else {
       return 'a';
     }
   }
 
-  Future<LugarModel> detectar(String fileName) async {
+  Future<Lugar> detectar(String fileName) async {
     var response = await http.post(
-      Uri.parse('http://192.168.1.2:4000/api/detector/'),
+      Uri.parse('https://holotourism.herokuapp.com/api/detector/'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -31,13 +36,21 @@ class Imagen {
         'fileName': fileName,
       }),
     );
-
-    if (response.statusCode == 200){
-      return LugarModel.fromJson(jsonDecode(response.body));
+    getDescriptions(jsonDecode(response.body));
+    if (response.statusCode == 200) {
+      return Lugar.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to post');
+      throw Exception('Failed to load album');
     }
   }
 
+  getDescriptions(Map<String, dynamic> json) async {
+    String nombre = json['nombre'].toString();
+    var url = Uri.parse(
+        'https://holotourism.herokuapp.com/api/description/${nombre}');
+    var response = await http.get(url);
+    print(json['nombre'].toString() + " nombre");
+    print('status: ${response.statusCode}');
+    print('body: ${response.body}');
+  }
 }
-
