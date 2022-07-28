14a5aa8151e449567107ab1504354d1cab65de64
Nestor Soto <nestor.soto@unmsm.edu.pe>
2022-07-28 14:26:16 -0500

    implementación de backend online

--- HoloTourism/lib/resources/Imagen.dart
+++ HoloTourism/lib/resources/Imagen.dart
@@ -7,8 +7,8 @@ import 'dart:io';
 
 class Imagen {
   Future<String> subir(String field, String filePath) async {
-    var request = http.MultipartRequest(
-        "POST", Uri.parse('http://192.168.56.1:4000/api/images/saveimg/'));
+    var request = http.MultipartRequest("POST",
+        Uri.parse('https://holotourism.herokuapp.com/api/images/saveimg/'));
     request.fields["file"] = field;
     request.files.add(await http.MultipartFile.fromPath("file", filePath));
     request.headers.addAll({"Content-type": "multipart/form-data"});
@@ -22,7 +22,7 @@ class Imagen {
 
   Future<LugarModel> detectar(String fileName) async {
     var response = await http.post(
-      Uri.parse('http://192.168.56.1:4000/api/detector/'),
+      Uri.parse('https://holotourism.herokuapp.com/api/detector/'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -43,7 +43,8 @@ class Imagen {
 
   static Future<String> getDescriptions(Map<String, dynamic> json) async {
     String nombre = json['nombre'].toString();
-    var url = Uri.parse('http://192.168.56.1:4000/api/description/${nombre}');
+    var url = Uri.parse(
+        'https://holotourism.herokuapp.com/api/description/${nombre}');
     var response = await http.get(url);
     //print(json['nombre'].toString() + " nombre");
     //print('status: ${response.statusCode}');
